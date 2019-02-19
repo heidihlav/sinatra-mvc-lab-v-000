8a423ce9bbb2dcbac5e723ba3f34ba1647b9ca5a
Heidi Hlavinka <hchlav@gmail.com>
2019-02-19 00:57:21 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ class App < Sinatra::Base
 
   post '/piglatinize' do
     piglatin_word = PigLatinizer.new
-    @piglatin = piglatin_word.piglatinize(params[user_phrase])
+    @piglatin = piglatin_word.piglatinize(params[:user_phrase])
     erb :results
 
   end
